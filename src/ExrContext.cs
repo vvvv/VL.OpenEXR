569a793e2c6e6c355d0ea2e88ed3bfde48e10823
Elias Holzer <elias@vvvv.org>
2025-05-12 23:38:42 +0200

    okay

--- src/ExrContext.cs
+++ src/ExrContext.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Buffers;
+using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using OpenEXR;
 using OpenEXR.Interop;
 using Stride.Core.Mathematics;
 using static OpenEXR.Interop.exr;
@@ -54,139 +57,261 @@ public unsafe class ExrContext : SafeHandle
         }
     }
 
-    public Rectangle GetDataWindow(int partIndex)
+    public ExrPart GetPart(int index)
+    {
+        if (index < 0 || index >= Count)
+            throw new ArgumentOutOfRangeException(nameof(index), $"Part index {index} is out of range [0, {Count})");
+        return new ExrPart(this, index);
+    }
+}
+
+public unsafe class ExrPart(ExrContext context, int partIndex)
+{
+    exr_context_t* Handle => context.handle;
+    int PartIndex => partIndex;
+
+    public Rectangle GetDataWindow()
     {
         exr_attr_box2i_t box;
-        exr_get_data_window(handle, partIndex, &box).ThrowIfError();
+        exr_get_data_window(Handle, PartIndex, &box).ThrowIfError();
         return new Rectangle(box.min.x, box.min.y, box.max.x - box.min.x + 1, box.max.y - box.min.y + 1);
     }
 
-    public exr_storage_t GetStorage(int partIndex)
+    public exr_storage_t GetStorage()
     {
         exr_storage_t result;
-        exr_get_storage(handle, partIndex, &result).ThrowIfError();
+        exr_get_storage(Handle, PartIndex, &result).ThrowIfError();
         return result;
     }
 
-    public int GetChannelCount(int partIndex)
+    public int GetChannelCount()
     {
         exr_attr_chlist_t* channels;
-        exr_get_channels(handle, partIndex, &channels).ThrowIfError();
+        exr_get_channels(Handle, PartIndex, &channels).ThrowIfError();
         return channels->num_channels;
     }
 
-    public int GetScanlinesPerChunk(int partIndex)
+    public unsafe exr_pixel_type_t GetPixelType(int channelIndex)
+    {
+        exr_attr_chlist_t* channels;
+        exr_get_channels(Handle, PartIndex, &channels).ThrowIfError();
+        if (channelIndex < 0 || channelIndex >= channels->num_channels)
+            throw new ArgumentOutOfRangeException(nameof(channelIndex), $"Channel index {channelIndex} is out of range [0, {channels->num_channels})");
+        var channel = channels->entries[channelIndex];
+        return channel.pixel_type;
+    }
+
+    public int GetScanlinesPerChunk()
     {
         int result;
-        exr_get_scanlines_per_chunk(handle, partIndex, &result).ThrowIfError();
+        exr_get_scanlines_per_chunk(Handle, PartIndex, &result).ThrowIfError();
         return result;
     }
 
-    public exr_chunk_info_t ReadScanlineChunkInfo(int partIndex, int y)
+    public exr_chunk_info_t ReadScanlineChunkInfo(int y)
     {
         exr_chunk_info_t result;
-        exr_read_scanline_chunk_info(handle, partIndex, y, &result).ThrowIfError();
+        exr_read_scanline_chunk_info(Handle, PartIndex, y, &result).ThrowIfError();
         return result;
     }
 
-    public IMemoryOwner<Color4> GetData(int partIndex)
+    public IMemoryOwner<T> Decode<T>(ExrChannel[] selectedChannels) where T : unmanaged, IElement<T>
     {
-        Debug.Assert(GetStorage(partIndex) == exr_storage_t.EXR_STORAGE_SCANLINE);
+        var storage = GetStorage();
+        if (storage != exr_storage_t.EXR_STORAGE_SCANLINE)
+            throw new NotSupportedException($"Unsupported storage type: {storage}");
 
-        var window = GetDataWindow(partIndex);
-        var scanlines = GetScanlinesPerChunk(partIndex);
-        var channelCount = GetChannelCount(partIndex);
-        var memoryOwner = MemoryPool<Color4>.Shared.Rent(window.Width * window.Height);
-        var memory = memoryOwner.Memory;
-
-        Parallel.ForEach(Partitioner.Create(window.Y, window.Y + window.Height, scanlines), t =>
+        var window = GetDataWindow();
+        var memoryOwner = MemoryPool<T>.Shared.Rent(window.Width * window.Height * selectedChannels.Length);
+        fixed (T* data = memoryOwner.Memory.Span)
         {
-            var (y, r) = t;
-            var dstY = y - window.Y;
-
-            var chunkInfo = ReadScanlineChunkInfo(partIndex, y);
-
-            exr_decode_pipeline_t decoder;
-            exr_decoding_initialize(handle, partIndex, &chunkInfo, &decoder).ThrowIfError();
-
-            fixed (Color4* data = memory.Span)
-            {
-                for (int c = 0; c < decoder.channel_count; c++)
-                {
-                    ref var channel = ref decoder.channels[c];
-                    var dstC = GetDstChannel(ref channel, c);
-                    channel.user_bytes_per_element = sizeof(float);
-                    channel.user_data_type = (ushort)exr_pixel_type_t.EXR_PIXEL_FLOAT;
-                    channel.user_pixel_stride = sizeof(Color4);
-                    channel.user_line_stride = window.Width * sizeof(Color4);
-                    channel.decode_to_ptr = (byte*)(data + dstY * window.Width) + (dstC * sizeof(float));
-                }
-            }
-
-            exr_decoding_choose_default_routines(handle, partIndex, &decoder).ThrowIfError();
-            exr_decoding_run(handle, partIndex, &decoder).ThrowIfError();
-
-            exr_decoding_destroy(handle, &decoder).ThrowIfError();
-        });
-        //var dstY = 0;
-        //for (int y = window.Y; y < window.Y + window.Height; y += scanlines, dstY += scanlines)
-        //{
-        //    var chunkInfo = ReadScanlineChunkInfo(partIndex, y);
-
-        //    exr_decode_pipeline_t decoder;
-        //    exr_decoding_initialize(handle, partIndex, &chunkInfo, &decoder).ThrowIfError();
-
-        //    fixed (Color4* data = memory.Span)
-        //    {
-        //        for (int c = 0; c < decoder.channel_count; c++)
-        //        {
-        //            ref var channel = ref decoder.channels[c];
-        //            var dstC = GetDstChannel(ref channel, c);
-        //            channel.user_bytes_per_element = sizeof(float);
-        //            channel.user_data_type = (ushort)exr_pixel_type_t.EXR_PIXEL_FLOAT;
-        //            channel.user_pixel_stride = sizeof(Color4);
-        //            channel.user_line_stride = window.Width * sizeof(Color4);
-        //            channel.decode_to_ptr = (byte*)(data + dstY * window.Width) + (dstC * sizeof(float));
-        //        }
-        //    }
-
-        //    exr_decoding_choose_default_routines(handle, partIndex, &decoder).ThrowIfError();
-        //    exr_decoding_run(handle, partIndex, &decoder).ThrowIfError();
-
-        //    exr_decoding_destroy(handle, &decoder).ThrowIfError();
-        //}
-
+            Decode(data, window, selectedChannels);
+        }
         return memoryOwner;
+    }
 
-        static int GetDstChannel(ref exr_coding_channel_info_t info, int i)
+    private void Decode<T>(T* data, Rectangle window, ExrChannel[] selectedChannels)
+        where T : unmanaged, IElement<T>
+    {
+        var scanlines = GetScanlinesPerChunk();
+        var elementCount = selectedChannels.Length;
+        var channelCount = GetChannelCount();
+
+        var chunkInfo = ReadScanlineChunkInfo(window.Y);
+
+        Parallel.ForEach(
+            source: Partitioner.Create(window.Y, window.Y + window.Height, scanlines),
+            localInit: () =>
+            {
+                var d = Marshal.AllocHGlobal(sizeof(exr_decode_pipeline_t));
+                var ci = chunkInfo;
+                var decoder = (exr_decode_pipeline_t*)d;
+                exr_decoding_initialize(Handle, PartIndex, &ci, decoder).ThrowIfError();
+                exr_decoding_choose_default_routines(Handle, PartIndex, decoder).ThrowIfError();
+                return d;
+            },
+            body: (t, s, d) =>
+            {
+                var decoder = (exr_decode_pipeline_t*)d;
+                var (y0, y1) = t;
+                var dstY0 = y0 - window.Y;
+                var dstY1 = y1 - window.Y;
+                var dstData0 = data + dstY0 * window.Width * elementCount;
+                var dstData1 = data + dstY1 * window.Width * elementCount;
+
+                var chunkInfo = ReadScanlineChunkInfo(y0);
+
+                exr_decoding_update(Handle, PartIndex, &chunkInfo, decoder).ThrowIfError();
+
+                for (int c = 0; c < decoder->channel_count; c++)
+                {
+                    ref var channel = ref decoder->channels[c];
+                    var i = GetElementIndex(selectedChannels, c);
+                    if (i < 0)
+                    {
+                        channel.decode_to_ptr = null;
+                        continue;
+                    }
+
+                    channel.user_bytes_per_element = (short)sizeof(T);
+                    channel.user_data_type = (ushort)T.PixelType;
+                    channel.user_pixel_stride = elementCount * sizeof(T);
+                    channel.user_line_stride = window.Width * channel.user_pixel_stride;
+                    channel.decode_to_ptr = (byte*)(dstData0 + i);
+                }
+
+                // Fill remaining channels with ones
+                for (int c = 0; c < selectedChannels.Length; c++)
+                {
+                    if (selectedChannels[c].Index >= 0)
+                        continue;
+
+                    for (var dst = dstData0 + c; dst < dstData1; dst += channelCount)
+                        *dst = T.One;
+                }
+
+                exr_decoding_run(Handle, PartIndex, decoder).ThrowIfError();
+
+                return d;
+            },
+            localFinally: d =>
+            {
+                var decoder = (exr_decode_pipeline_t*)d;
+                exr_decoding_destroy(Handle, decoder).ThrowIfError();
+                Marshal.FreeHGlobal(d);
+            });
+
+        static int GetElementIndex(ExrChannel[] channels, int index)
         {
-            var c = new ReadOnlySpan<char>(info.channel_name, 1);
-            return c switch
+            for (int i = 0; i < channels.Length; i++)
+            {
+                if (channels[i].Index == index)
+                    return i;
+            }
+            return -1;
+        }
+    }
+
+    public HashSet<string> GetLayers()
+    {
+        var layers = new HashSet<string>();
+        var channels = GetChannels();
+        foreach (var c in channels)
+        {
+            var l = c.Layer;
+            if (l != string.Empty)
+                layers.Add(l);
+        }
+        return layers;
+    }
+
+    public unsafe ExrChannel[] GetChannels()
+    {
+        exr_attr_chlist_t* channels;
+        exr_get_channels(Handle, partIndex, &channels).ThrowIfError();
+        var result = new ExrChannel[channels->num_channels];
+        for (int i = 0; i < channels->num_channels; i++)
+        {
+            var channel = channels->entries[i];
+            var name = new string(channel.name.str, 0, channel.name.length);
+            result[i] = new ExrChannel(i, name, channel.pixel_type);
+        }
+        return result;
+    }
+
+    public ExrChannel[] GetChannels(string layer)
+    {
+        var channels = GetChannels();
+        var result = new List<ExrChannel>();
+        foreach (var c in channels)
+        {
+            if (c.Name.StartsWith(layer))
+                result.Add(c);
+        }
+        return result.ToArray();
+    }
+}
+
+public interface IElement<T> where T : unmanaged, IElement<T>
+{
+    static abstract exr_pixel_type_t PixelType { get; }
+    static abstract T One { get; }
+}
+
+record struct FloatElement(float Value) : IElement<FloatElement>
+{
+    public static exr_pixel_type_t PixelType => exr_pixel_type_t.EXR_PIXEL_FLOAT;
+    public static FloatElement One => new FloatElement(1.0f);
+}
+
+record struct HalfElement(ushort Value) : IElement<HalfElement>
+{
+    public static exr_pixel_type_t PixelType => exr_pixel_type_t.EXR_PIXEL_HALF;
+    public static HalfElement One => new HalfElement(0x3C00);
+}
+
+record struct UIntElement(uint Value) : IElement<UIntElement>
+{
+    public static exr_pixel_type_t PixelType => exr_pixel_type_t.EXR_PIXEL_UINT;
+    public static UIntElement One => new UIntElement(1);
+}
+
+public record struct ExrChannel(int Index, string Name, exr_pixel_type_t PixelType)
+{
+    public string Layer
+    {
+        get
+        {
+            var index = Name.LastIndexOf('.');
+            if (index > 0)
+                return Name.Substring(0, index);
+            return string.Empty;
+        }
+    }
+
+    public string Suffix
+    {
+        get
+        {
+            var index = Name.LastIndexOf('.');
+            if (index >= 0 && index < Name.Length)
+                return Name.Substring(index + 1);
+            return Name;
+        }
+    }
+
+    public int Order
+    {
+        get
+        {
+            return Suffix switch
             {
                 "R" => 0,
                 "G" => 1,
                 "B" => 2,
                 "A" => 3,
-                _ => i
+                _ => int.MaxValue,
             };
         }
-    }
-}
-
-public unsafe struct ExrDecodePipeline : IDisposable
-{
-    private readonly exr_context_t* context;
-    private readonly exr_decode_pipeline_t native;
-
-    public ExrDecodePipeline(exr_context_t* context, exr_decode_pipeline_t native)
-    {
-        this.context = context;
-        this.native = native;
-    }
-
-    public void Dispose()
-    {
-        var native = this.native;
-        exr_decoding_destroy(context, &native).ThrowIfError();
     }
 }
--- src/VL.OpenEXR.cs
+++ src/VL.OpenEXR.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using Stride.Core.Mathematics;
 using Stride.Graphics;
@@ -44,43 +45,16 @@ namespace OpenEXR
             if (useOpenEXRCore)
                 return LoadFromPathOpenEXRCore(path, device);
 
-            ExrPixelFormat exrFormat;
-            PixelFormat format;
-            IntPtr ptr;
-            var result = load_from_path(path, out var width, out var height, out var numChannels, out exrFormat, out ptr);
+            var result = load_from_path(path, out var width, out var height, out var numChannels, out var exrFormat, out var ptr);
 
-            if(result != 0) {
-                format = PixelFormat.None;
+            if (result != 0) 
                 return null;
-            }
 
-            if(exrFormat == ExrPixelFormat.Unknown || ptr == IntPtr.Zero)
-            {
-                format = PixelFormat.None;
+            if (exrFormat == ExrPixelFormat.Unknown || ptr == IntPtr.Zero)
                 return null;
-            }
 
-            int sizeInBytes = 0;
-            (format, sizeInBytes) = (exrFormat, numChannels) switch
-            {
-                (ExrPixelFormat.F16, 4) => (PixelFormat.R16G16B16A16_Float, 2),
-                (ExrPixelFormat.F32, 4) => (PixelFormat.R32G32B32A32_Float, 4),
-                (ExrPixelFormat.U32, 4) => (PixelFormat.R32G32B32A32_UInt , 4),
-
-                (ExrPixelFormat.F32, 3) => (PixelFormat.R32G32B32_Float, 4),
-                (ExrPixelFormat.U32, 3) => (PixelFormat.R32G32B32_UInt , 4),
-
-                (ExrPixelFormat.F16, 2) => (PixelFormat.R16G16_Float, 2),
-                (ExrPixelFormat.F32, 2) => (PixelFormat.R32G32_Float, 4),
-                (ExrPixelFormat.U32, 2) => (PixelFormat.R32G32_UInt , 4),
-
-                (ExrPixelFormat.F16, 1) => (PixelFormat.R16_Float, 2),
-                (ExrPixelFormat.F32, 1) => (PixelFormat.R32_Float, 4),
-                (ExrPixelFormat.U32, 1) => (PixelFormat.R32_UInt , 4),
-                _ => (PixelFormat.None, 0),
-            };
-
-            var rowPitch = width * numChannels * sizeInBytes;
+            var format = GetPixelFormat(exrFormat, numChannels);
+            var rowPitch = width * format.SizeInBytes();
 
             var texture = Texture.New(
                 device,
@@ -92,15 +66,63 @@ namespace OpenEXR
             return texture;
         }
 
-        private static unsafe Texture LoadFromPathOpenEXRCore(string path, GraphicsDevice device)
+        private static PixelFormat GetPixelFormat(ExrPixelFormat exrFormat, int numChannels)
+        {
+            return (exrFormat, numChannels) switch
+            {
+                (ExrPixelFormat.F16, 4) => PixelFormat.R16G16B16A16_Float,
+                (ExrPixelFormat.F32, 4) => PixelFormat.R32G32B32A32_Float,
+                (ExrPixelFormat.U32, 4) => PixelFormat.R32G32B32A32_UInt,
+                (ExrPixelFormat.F16, 3) => PixelFormat.R16G16B16A16_Float,
+                (ExrPixelFormat.F32, 3) => PixelFormat.R32G32B32_Float,
+                (ExrPixelFormat.U32, 3) => PixelFormat.R32G32B32_UInt,
+                (ExrPixelFormat.F16, 2) => PixelFormat.R16G16_Float,
+                (ExrPixelFormat.F32, 2) => PixelFormat.R32G32_Float,
+                (ExrPixelFormat.U32, 2) => PixelFormat.R32G32_UInt,
+                (ExrPixelFormat.F16, 1) => PixelFormat.R16_Float,
+                (ExrPixelFormat.F32, 1) => PixelFormat.R32_Float,
+                (ExrPixelFormat.U32, 1) => PixelFormat.R32_UInt,
+                _ => PixelFormat.None,
+            };
+        }
+
+        private static unsafe Texture LoadFromPathOpenEXRCore(string path, GraphicsDevice device, int partIndex = 0)
         {
             using var context = ExrContext.OpenRead(path);
-            var w = context.GetDataWindow(partIndex: 0);
-            using var memory = context.GetData(partIndex: 0);
-            using var handle = memory.Memory.Pin();
-            var rowPitch = w.Width * sizeof(Color4);
-            return Texture.New(device, TextureDescription.New2D(w.Width, w.Height, PixelFormat.R32G32B32A32_Float, usage: GraphicsResourceUsage.Immutable), 
-                new DataBox((nint)handle.Pointer, rowPitch, rowPitch * w.Height));
+            var part = context.GetPart(partIndex);
+            var selectedChannels = part.GetChannels()
+                .OrderBy(c => c.Order)
+                .Take(4)
+                .ToArray();
+
+            var exrFormat = (ExrPixelFormat)selectedChannels[0].PixelType;
+            var pixelFormat = GetPixelFormat(exrFormat, selectedChannels.Length);
+            if (pixelFormat == PixelFormat.R16G16B16A16_Float && selectedChannels.Length == 3)
+            {
+                // We use PixelFormat.R16G16B16A16_Float for 3 channels, so we need to add an alpha channel
+                selectedChannels = selectedChannels.Append(new ExrChannel(-1, "A", Interop.exr_pixel_type_t.EXR_PIXEL_HALF)).ToArray();
+            }
+
+            return exrFormat switch
+            { 
+                ExrPixelFormat.U32 => Decode<UIntElement>(),
+                ExrPixelFormat.F16 => Decode<HalfElement>(),
+                ExrPixelFormat.F32 => Decode<FloatElement>(),
+                _ => throw new NotImplementedException()
+            };
+
+            Texture Decode<T>() where T : unmanaged, IElement<T>
+            {
+                using var memory = part.Decode<T>(selectedChannels);
+                using var handle = memory.Memory.Pin();
+
+                var dataWindow = part.GetDataWindow();
+                var rowPitch = dataWindow.Width * pixelFormat.SizeInBytes();
+                return Texture.New(
+                    device, 
+                    TextureDescription.New2D(dataWindow.Width, dataWindow.Height, pixelFormat, usage: GraphicsResourceUsage.Immutable),
+                    new DataBox((nint)handle.Pointer, rowPitch, rowPitch * dataWindow.Height));
+            }
         }
     }
 
